5a4d9148aa8274af20827dc649db9e82a5b7a53d
nghiaphung <ducnghia318@gmail.com>
2017-10-03 18:09:17 +0700

    lung tung

--- 2_workspace/test_firmware/driver/lcd/lcd.c
+++ 2_workspace/test_firmware/driver/lcd/lcd.c
@@ -8,6 +8,8 @@
 /******************************************************************************/
 /**!                               INCLUDE                                    */
 /******************************************************************************/
+#include "stm32f10x_rcc.h"
+#include "stm32f10x_gpio.h"
 #include "lcd.h"
 /******************************************************************************/
 /**!                            LOCAL TYPEDEF                                 */
@@ -37,6 +39,10 @@
 #define LCD_RS_PORT           GPIOA
 #define LCD_EN_PORT           GPIOB
 #define LCD_RW_PORT           GPIOB
+
+#define  SYS_TIMER_CLK        (1000000UL)
+
+
 /******************************************************************************/
 /**!                            LOCAL SYMBOLS                                 */
 /******************************************************************************/
@@ -52,7 +58,28 @@
 /******************************************************************************/
 /**!                    LOCAL FUNCTIONS PROTOTYPES                            */
 /******************************************************************************/
-
+void LCD_SetDB0(void);
+void LCD_SetDB1(void);
+void LCD_SetDB2(void);
+void LCD_SetDB3(void);
+void LCD_SetDB4(void);
+void LCD_SetDB5(void);
+void LCD_SetDB6(void);
+void LCD_SetDB7(void);
+void LCD_ResetDB0 (void);
+void LCD_ResetDB1 (void);
+void LCD_ResetDB2 (void);
+void LCD_ResetDB3 (void);
+void LCD_ResetDB4 (void);
+void LCD_ResetDB5 (void);
+void LCD_ResetDB6 (void);
+void LCD_ResetDB7 (void);
+void LCD_SetEN (void);
+void LCD_ResetEN (void);
+void LCD_SetRW (void);
+void LCD_ResetRW (void);
+void LCD_SetRS (void);
+void LCD_ResetRS (void);
 /******************************************************************************/
 /**!                        EXPORTED FUNCTIONS                                */
 /******************************************************************************/
@@ -94,8 +121,128 @@ void LCD_Init (void)
     /* initialize EN */
     GPIO_InitStruct.GPIO_Pin     = LCD_EN_PIN;
     GPIO_Init(LCD_EN_PORT, &GPIO_InitStruct);
-    
 }
+
+
 /******************************************************************************/
 /**!                          LOCAL FUNCTIONS                                 */
 /******************************************************************************/
+void LCD_DelayUs (uint32_t pUs)
+{
+    RCC_ClocksTypeDef           RCC_ClockStatus    = {0};
+    
+    /* get clock status */
+    RCC_GetClocksFreq(&RCC_ClockStatus);
+    pUs = RCC_ClockStatus.SYSCLK_Frequency / (SYS_TIMER_CLK*3);
+    for (uint32_t i; i < pUs; i++);
+}
+
+void LCD_SetDB0(void)
+{
+    GPIO_WriteBit(LCD_DB0_PORT, LCD_DB0_PIN, Bit_SET);
+}
+
+void LCD_SetDB1(void)
+{
+    GPIO_WriteBit(LCD_DB1_PORT, LCD_DB1_PIN, Bit_SET);
+}
+
+void LCD_SetDB2(void)
+{
+    GPIO_WriteBit(LCD_DB2_PORT, LCD_DB2_PIN, Bit_SET);
+}
+
+void LCD_SetDB3(void)
+{
+    GPIO_WriteBit(LCD_DB3_PORT, LCD_DB3_PIN, Bit_SET);
+}
+
+void LCD_SetDB4(void)
+{
+    GPIO_WriteBit(LCD_DB4_PORT, LCD_DB4_PIN, Bit_SET);
+}
+
+void LCD_SetDB5(void)
+{
+    GPIO_WriteBit(LCD_DB5_PORT, LCD_DB5_PIN, Bit_SET);
+}
+
+void LCD_SetDB6(void)
+{
+    GPIO_WriteBit(LCD_DB6_PORT, LCD_DB6_PIN, Bit_SET);
+}
+
+void LCD_SetDB7(void)
+{
+    GPIO_WriteBit(LCD_DB7_PORT, LCD_DB7_PIN, Bit_SET);
+}
+
+void LCD_ResetDB0 (void)
+{
+    GPIO_WriteBit(LCD_DB0_PORT, LCD_DB0_PIN, Bit_RESET);
+}
+
+void LCD_ResetDB1 (void)
+{
+    GPIO_WriteBit(LCD_DB1_PORT, LCD_DB1_PIN, Bit_RESET);
+}
+
+void LCD_ResetDB2 (void)
+{
+    GPIO_WriteBit(LCD_DB2_PORT, LCD_DB2_PIN, Bit_RESET);
+}
+
+void LCD_ResetDB3 (void)
+{
+    GPIO_WriteBit(LCD_DB3_PORT, LCD_DB3_PIN, Bit_RESET);
+}
+
+void LCD_ResetDB4 (void)
+{
+    GPIO_WriteBit(LCD_DB4_PORT, LCD_DB4_PIN, Bit_RESET);
+}
+
+void LCD_ResetDB5 (void)
+{
+    GPIO_WriteBit(LCD_DB5_PORT, LCD_DB5_PIN, Bit_RESET);
+}
+
+void LCD_ResetDB6 (void)
+{
+    GPIO_WriteBit(LCD_DB6_PORT, LCD_DB6_PIN, Bit_RESET);
+}
+
+void LCD_ResetDB7 (void)
+{
+    GPIO_WriteBit(LCD_DB7_PORT, LCD_DB7_PIN, Bit_RESET);
+}
+
+void LCD_SetEN (void)
+{
+    GPIO_WriteBit(LCD_EN_PORT, LCD_EN_PIN, Bit_SET);
+}
+
+void LCD_ResetEN (void)
+{
+    GPIO_WriteBit(LCD_EN_PORT, LCD_EN_PIN, Bit_RESET);
+}
+
+void LCD_SetRW (void)
+{
+    GPIO_WriteBit(LCD_RW_PORT, LCD_RW_PIN, Bit_SET);
+}
+
+void LCD_ResetRW (void)
+{
+    GPIO_WriteBit (LCD_RW_PORT, LCD_RW_PIN, Bit_RESET);
+}
+
+void LCD_SetRS (void)
+{
+    GPIO_WriteBit(LCD_RS_PORT, LCD_RS_PIN, Bit_SET);
+}
+
+void LCD_ResetRS (void)
+{
+    GPIO_WriteBit(LCD_RS_PORT, LCD_RS_PIN, Bit_RESET);
+}
--- 2_workspace/test_firmware/driver/timer/timer.c
+++ 2_workspace/test_firmware/driver/timer/timer.c
@@ -1,18 +1,36 @@
 /*
  * @filename: timer.c
- * @Author	: nghiaphung
+ * @Author	: 
  * @Date	: 30/9/2017
- * @Email	: ducnghia318@gmail.com
+ * @Email	: 
  */
  
 /******************************************************************************/
 /**!                               INCLUDE                                    */
 /******************************************************************************/
+#include "stm32f10x_rcc.h"
+#include "stm32f10x_tim.h"
 #include "timer.h"
 /******************************************************************************/
 /**!                            LOCAL TYPEDEF                                 */
 /******************************************************************************/
+/**
+ * Software timer
+ */
+typedef struct
+{
+    uint32_t Interval;
+    uint32_t Timeout;
+    uint8_t  Started;
+    void     (*callback)(void*);
+    void*    Param;
+}SWTimer_t;
 
+#define  SYS_TIMER_UNIT              TIM4
+#define  SYS_TIMER_CLK               (1000000UL)
+#define  SYS_TIMER_IRQ               TIM4_IRQn
+#define  SYS_TIMER_MAX_SW_TIMER      10
+#define  NULL                        ((void*)0)
 /******************************************************************************/
 /**!                            LOCAL SYMBOLS                                 */
 /******************************************************************************/
@@ -24,7 +42,9 @@
 /******************************************************************************/
 /**!                          LOCAL VARIABLES                                 */
 /******************************************************************************/
-
+static volatile uint32_t _xSystemTimerMsCounter = 0;
+static volatile uint8_t  _xSystemSWTimersNum = 0;
+static SWTimer_t         _xTimers[SYS_TIMER_MAX_SW_TIMER] = {{0}};
 /******************************************************************************/
 /**!                    LOCAL FUNCTIONS PROTOTYPES                            */
 /******************************************************************************/
@@ -32,7 +52,137 @@
 /******************************************************************************/
 /**!                        EXPORTED FUNCTIONS                                */
 /******************************************************************************/
+void SysTimer_Init (void)
+{
+    TIM_TypeDef*                TIMx = NULL;   
+    uint16_t                    TIM_Prescaler = 0;
+    TIM_TimeBaseInitTypeDef     TIM_TimeBaseInitStruct ={0};
+    NVIC_InitTypeDef            NVIC_InitStruct    = {0};
+    RCC_ClocksTypeDef           RCC_ClockStatus    = {0};
+    
+    /* get clock status */
+    RCC_GetClocksFreq(&RCC_ClockStatus);
+    /* Calculate pre-scaler */
+    TIM_Prescaler = (uint16_t)((RCC_ClockStatus.PCLK1_Frequency)/SYS_TIMER_CLK);
+    /* setup timer's param */
+    TIMx  = SYS_TIMER_UNIT;
+    TIM_TimeBaseInitStruct.TIM_ClockDivision     = TIM_CKD_DIV1;
+    TIM_TimeBaseInitStruct.TIM_CounterMode       = TIM_CounterMode_Up;
+    TIM_TimeBaseInitStruct.TIM_Period            = 1000;   // 1000us
+    TIM_TimeBaseInitStruct.TIM_Prescaler         = TIM_Prescaler;
+    TIM_TimeBaseInitStruct.TIM_RepetitionCounter = 0;
+    TIM_TimeBaseInit(TIMx, &TIM_TimeBaseInitStruct);
+    TIM_ITConfig(TIMx, TIM_IT_Update, ENABLE);
+    /*  initialize NVIC */
+    NVIC_InitStruct.NVIC_IRQChannel             = SYS_TIMER_IRQ;
+    NVIC_InitStruct.NVIC_IRQChannelCmd          = ENABLE;
+    NVIC_InitStruct.NVIC_IRQChannelSubPriority  = 2;
+    NVIC_Init(&NVIC_InitStruct);
+    /* reset all variable to default */
+    _xSystemTimerMsCounter = 0;
+    _xSystemSWTimersNum    = 0;
+}
 
+/**
+ * @brief Create software timer unit
+ * @param xInterval
+ * @param Callback
+ * @param xParam
+ * @return
+ *          -1: fail to create software timer
+ *          0..SYS_TIMER_NUM_SW_TIMER_MAX: success
+ */
+int Timer_CreateSWTimer (uint32_t pInterval, void (*callback)(void*), void* pParam)
+{
+    if ((0 == pInterval) || (NULL == callback))
+        return -1;
+    int i = 0;
+    for (i = 0; i < SYS_TIMER_MAX_SW_TIMER; i++)
+    {
+        if (_xTimers[i].callback == NULL)
+        {
+            _xTimers[i].Interval  = pInterval;
+            _xTimers[i].Timeout   = 0;
+            _xTimers[i].Param     = pParam;
+            _xTimers[i].callback  = callback;
+            _xSystemSWTimersNum++;
+            return i;
+        }
+    }
+    return -1;
+}
+
+/**
+ * @brief Delete indicated software timer
+ * @param pTimerID
+ * @return
+ */
+
+int Timer_DeleteSWTimer (int pTimerID)
+{
+    if ((pTimerID >= SYS_TIMER_MAX_SW_TIMER) || (pTimerID < 0))
+        return -1;
+    if (_xTimers[pTimerID].callback != NULL)
+    {
+        _xTimers[pTimerID].callback = NULL;
+        _xTimers[pTimerID].Param    = NULL;
+    }
+    return 0;
+}
+
+/**
+ * @brief Start indicated software timer
+ * @param xTimerId
+ * @return
+ *          0: success
+ *          -1 : fail
+ */
+int Tiemr_RunSWTimer (int pTimerID)
+{
+    if ((pTimerID >= SYS_TIMER_MAX_SW_TIMER) || (pTimerID < 0))
+        return -1;
+    if (_xTimers[pTimerID].callback != NULL)
+    {
+        /* Get current time-stamp */        
+        _xTimers[pTimerID].Timeout = Timer_GetSysMsTimer();
+        /* Calculate timeout point */        
+        _xTimers[pTimerID].Timeout += _xTimers[pTimerID].Interval;
+        /* Indicate software timer has been started */        
+        _xTimers[pTimerID].Started = 1;
+    }
+    return 0;
+}
+
+/**
+ * @brief To stop indicated timer
+ * @param xTimerId
+ * @return
+ *          0: success
+ *          -1 : fail
+ */
+int Timer_HaltSWTimer(int pTimerID)
+{
+    /* Check error */
+    if ((pTimerID >= SYS_TIMER_MAX_SW_TIMER) || (pTimerID < 0))
+        return -1;
+    /* Query timers list */
+    if (_xTimers[pTimerID].callback != NULL) 
+    {
+        /* Indicate software timer has been started */
+        _xTimers[pTimerID].Started  = 0;
+        return 0;
+    }
+    return -1;
+}
+
+/**
+ * @brief Get system timer counter value in milliseconds
+ * @return
+ */
+uint32_t Timer_GetSysMsTimer(void)
+{
+    return _xSystemTimerMsCounter;
+}
 /******************************************************************************/
 /**!                          LOCAL FUNCTIONS                                 */
 /******************************************************************************/
